702b32cf6e51196b8c7bf56272b0185a26d1ffe8
Isaac Sotelo <sotelocreate@gmail.com>
2016-02-22 12:34:11 -0700

    Adjusted to fit the driver's new y-axis

--- bin_packing.py
+++ bin_packing.py
@@ -1,6 +1,23 @@
-import math
+# ----------------------------------------------
+# CSCI 338, Spring 2016, Bin Packing Assignment
+# Author: Danny Kumpf and Isaac Sotelo
+# Last Modified: February 17, 2016
+# ----------------------------------------------
 
-def mergeSort(alist):
+"""
+BIN_PACKING_SOLUTION:
+    We sorted the list of rectangles from smallest to largest. Then they were placed into columns until they reached a cuttoff limit and then a new column is started at the top. It is continued until the list is empty. Our cuttoff limit value is based on finding the average rectangle size using the total x and total y values. A traversing method was used to find these values. Which is then multiplied by the average number of rectangles per column. 
+--------------------------------------------------
+RETURNS: a list of tuples that designate the top left corner placement,
+         e.g. [(x1, y1), ... (xn, yn)] where
+         x1 = top left x coordinate of rectangle 1 placement
+
+
+"""
+
+import math #used for the square root function
+
+def mergeSort(alist): #sorts the list of tuples to be ordered from lowest to highest (x and y)
     if len(alist)>1:
         mid = len(alist)//2
         lefthalf = alist[:mid]
@@ -31,64 +48,69 @@ def mergeSort(alist):
             j=j+1
             k=k+1
     
-#maybe not need counter
-def find_solution(sizes):
-	mergeSort(sizes)
-	measurements = traverse(sizes)
-	#print (measurements)
-	#print(sizes)
-	cutoff = len(sizes)
-	##print(cutoff)
+def findCutoff(sizes):
 
-	cutoff = (int)(math.sqrt(cutoff))
-	#find the average length of xTotal and yTotal and use it here instead of measurements[0]
-	avgXandY = (measurements[0]+measurements[1])/2
-	##print (avgXandY)
+	measurements = traverse(sizes)					#Go through the list and return the total x and y values							
 
-	avgSize = (avgXandY)/len(sizes)
-	#print("avgSize: ")
-	#print(avgSize)
+	avgCol = (int)(math.sqrt(len(sizes)))			#gets the average number of rectangles per column
+	
+	total = measurements[0]+measurements[1]
+	avgXandY = (total)/2							#average value of the total x's and y's
 
-	avgCol = avgSize*cutoff
+	avgSize = (avgXandY)/len(sizes)					#average size of each rectangle
 
-	#printing the set farthest y value
-	#print (avgCol)
-	#print(len(sizes))
-	t_y = 0
-	t_x = 0
-	#counter = 0
-	outerIndexX = 0
-	biggest = 0
-	res = []
-	sizes.reverse()
-	for i in range(0,len(sizes)):
+	cutoff = avgSize*avgCol							#the cutoff length of the column
+
+	return cutoff
+
+def find_solution(sizes): 
+
+	table = {}										#keep track of the id of the rectangles and index it was originally at
+	t_y = 0											#top left y coordinate
+	t_x = 0											#top left x coordinate
+	outerIndexX = 0									#keeps track of where the new column should start
+	biggestX = 0									#temporary variable to find outerIndexX should be
+	res = [0]*len(sizes)							#resulting list to return
+
+	for i in range(0,len(sizes)):					#putting the id and Idex in a dictionary directly
+		table[id(sizes[i])] = i
+
+	mergeSort(sizes)								#ordering the list of rectangles
+
+	cutoff = findCutoff(sizes)
+
+	sizes.reverse()									#reverse the list so that the larger rectangles will be placed first
+
+	for i in range(0,len(sizes)):				#find the coordinates for each rectangle
 		
-		if (sizes[i][0] > biggest):
-			biggest = sizes[i][0]
+		if (sizes[i][0] > biggestX):			#find the biggest x value
+			biggestX = sizes[i][0]
 
+		coordinate = (t_x, t_y)
 
-		res.append((t_x, t_y))
-		t_y += sizes[i][1]
-		#counter = counter + 1
+		realIndex = table[id(sizes[i])]			#get the index of the current rectangle by looking it up in the dictionary using the id
+		
+		res[realIndex] = coordinate  			#add the rectangle coordinates to the resulting list 
 
-		if (t_y >= avgCol):
-			counter = 0
-			t_y = 0
-			outerIndexX += biggest
-			t_x = outerIndexX
-			biggest = 0
-	#printing the farthest x value
-	#print (res[len(sizes)-1][0])
+		t_y -= (sizes[i][1])					#update the y value to the y value of the current rectangle
+
+		if (t_y <= (cutoff*(-1))):				#checks whether a new column needs to be started
+			t_y = 0								#sets the y value back to zero
+			outerIndexX += (biggestX)			#sets the start of the new column
+			t_x = outerIndexX					
+			biggestX = 0
+
 	return res
-'''counter % cutoff == 0 or''' 
 
-def traverse(blist):
+def traverse(blist):  #returns each x and y value of the given list
 	
 	xTotal = 0
 	yTotal = 0
-	for i in range(0, len(blist)):
-		xTotal += blist[i][0]
-		yTotal += blist[i][1]
+
+	for i in range(0, len(blist)):	#go through the whole list
+		xTotal += blist[i][0]		#add up all the x values
+		yTotal += blist[i][1]		#add up all the y values
 
 	res = [xTotal,yTotal]
+	
 	return res
